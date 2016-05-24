6272db947d066be759e83059bcb60d9b252b676a
dunivint <dunivint@Taylors-MacBook-Pro-2.local>
2016-05-24 10:25:14 -0400

    Grofit files

---
+++ 20160421_Data_analysis.R
@@ -0,0 +1,257 @@
+library(grofit)
+library(reshape2)
+library(dplyr)
+library(ggplot2)
+
+#read in raw data
+data=data.frame(read.table("20160421_01_AsV_MIC_I2727,47,A2705.asc"), row.names=TRUE)
+
+
+#extract the time information, remove the extraneous "s"
+time=row.names(data)
+time=as.numeric(sub("s", "",time))
+row.names(data)=NULL
+
+#define negative controls and subtract from dataset
+neg0=data.frame(data[,12])
+neg0=data.frame(rep(neg0, 12))
+neg10=data.frame(data[,24])
+neg10=data.frame(rep(neg10, 12))
+neg50=data.frame(data[,36])
+neg50=data.frame(rep(neg50, 12))
+neg100=data.frame(data[,48])
+neg100=data.frame(rep(neg100, 12))
+neg150=data.frame(data[,60])
+neg150=data.frame(rep(neg150, 12))
+neg200=data.frame(data[,72])
+neg200=data.frame(rep(neg200, 9))
+neg200=data.frame(cbind(neg200,data[,12],data[,48]),data[,72])
+neg250=data.frame(data[,84])
+neg250=data.frame(rep(neg250,9))
+neg250=data.frame(cbind(neg250,data[,24],data[,60],data[,84]))
+neg300=data.frame(data[,96])
+neg300=data.frame(rep(neg300,9))
+neg300=data.frame(cbind(neg300,data[,36],data[,12],data[,96]))
+neg=data.frame(cbind(neg0,neg10,neg50,neg100,neg150,neg200,neg250,neg300))
+data= data - neg
+
+#make a time matrix based on number of observations and variables
+n=nrow(data)
+m=ncol(data)
+time=matrix(rep(time, m), c(n, m))
+time=t(time)
+time=data.frame(time)
+
+
+#transpose the data
+data=t(data)
+
+
+#read in platemap
+platemap=read.csv("20160421_Platemap_High_MIC.csv")
+
+#combine data with platemap and remove well column
+data=cbind(platemap, data)
+
+#remove well column as it is no longer necessary
+data=data[,-1]
+
+#make control file to incorporate grofit options
+control=grofit.control(nboot.gc=100, parameter = 31, nboot.dr=100)
+
+#OPTION: only run table 
+results=gcFit(time, data, control)
+
+#make a data frame of the results
+results=data.frame(summary(results))
+
+#save results table
+write.csv(results, "20160421_results")
+
+#Find EC50
+EC50=drFit(results, control)
+EC50=data.frame(summary(EC50))
+
+#save EC50 results
+write.csv(EC50, "20160421_EC50")
+
+#extract maximum growth parameter from dataset
+mu=data.frame(results$mu.spline, results$reliability)
+
+#add information to mu
+mu=cbind(platemap, mu)
+
+#add replicate number to data
+reps=data.frame(read.table("20160421_replicates.csv", header=TRUE))
+mu=cbind(mu, reps)
+
+#remove control values with replicate numbers
+normalization=subset(mu, Concentration %in% 0)
+
+#remove unimportant columns
+normalization=normalization[,-1]
+normalization=normalization[,-2]
+normalization=normalization[,-2]
+
+#add negatives back
+mu=inner_join(mu, normalization, by = c("Strain", "Replicate"))
+
+#normalize data
+norm=mu$results.mu.spline.x/mu$results.mu.spline.y
+norm=data.frame(norm)
+#add information to normalized data
+mu=cbind(mu, norm)
+
+#group data
+grouped=group_by(mu, Strain, Concentration)
+
+#remove tagged(bad) wells from data analysis
+grouped2=grouped[!(grouped$results.reliability.x=="FALSE"),]
+
+#Calculate average and stdev
+stats=summarise(grouped2, Average=mean(norm), StDev=sd(norm))
+
+#plot data
+ggplot(data=stats, aes(x=Concentration, y=Average)) +
+  geom_line(size=1) +
+  geom_point(aes(color=Concentration), size=2.5) +
+  geom_point(shape=1, size=2.5, color="black") +
+  geom_errorbar(aes(ymax=stats$Average + stats$StDev, ymin=stats$Average - stats$StDev)) +
+  scale_color_gradientn(colors=rainbow(6)) +
+  facet_wrap(~Strain)
+
+#Extract maximum OD590 and reliability data
+A=data.frame(results$A.spline, results$reliability)
+A=cbind(platemap, A)
+
+#add replicate number to data
+reps=data.frame(read.table("20160418_replicates.csv", header=TRUE))
+A=cbind(A, reps)
+
+#remove control values with replicate numbers
+normalization=subset(A, Concentration %in% 0)
+
+#remove unimportant columns
+normalization=normalization[,-1]
+normalization=normalization[,-2]
+normalization=normalization[,-2]
+
+#add negatives back
+A=inner_join(A, normalization, by = c("Strain", "Replicate"))
+
+#normalize data
+norm=A$results.A.spline.x/A$results.A.spline.y
+
+#add information to normalized data
+A=cbind(A, norm)
+
+#group data
+grouped=group_by(A, Strain, Concentration)
+
+#remove tagged(bad) wells from data analysis
+grouped2=grouped[!(grouped$results.reliability.x=="FALSE"),]
+
+#Calculate average and stdev
+stats=summarise(grouped2, Average=mean(norm), StDev=sd(norm))
+
+#plot data
+ggplot(data=stats, aes(x=Concentration, y=Average)) +
+  geom_line(size=1) +
+  geom_point(aes(color=Concentration), size=2.5) +
+  geom_point(shape=1, size=2.5, color="black") +
+  geom_errorbar(aes(ymax=stats$Average + stats$StDev, ymin=stats$Average - stats$StDev)) +
+  scale_color_gradientn(colors=rainbow(6)) +
+  facet_wrap(~Strain) +
+  ylim(0,1.1)
+
+#extract time to exponential growth parameter (lambda) from dataset
+lambda=data.frame(results$lambda.spline, results$reliability)
+
+#add information to mu
+lambda=cbind(platemap, lambda)
+
+#add replicate number to data
+reps=data.frame(read.table("20160418_replicates.csv", header=TRUE))
+lambda=cbind(lambda, reps)
+
+#remove control values with replicate numbers
+normalization=subset(lambda, Concentration %in% 0)
+
+#remove unimportant columns
+normalization=normalization[,-1]
+normalization=normalization[,-2]
+normalization=normalization[,-2]
+
+#add negatives back
+lambda=inner_join(lambda, normalization, by = c("Strain", "Replicate"))
+
+#normalize data
+norm=lambda$results.lambda.spline.x/lambda$results.lambda.spline.y
+
+#add information to normalized data
+lambda=cbind(lambda, norm)
+
+#group data
+grouped=group_by(lambda, Strain, Concentration)
+
+#remove tagged(bad) wells from data analysis
+grouped2=grouped[!(grouped$results.reliability.x=="FALSE"),]
+
+#Calculate average and stdev
+stats=summarise(grouped2, Average=mean(norm), StDev=sd(norm))
+
+
+#plot data
+ggplot(data=stats, aes(x=Concentration, y=Average)) +
+  geom_line(size=1.5) +
+  geom_point(aes(color=Concentration), size=2.5) +
+  geom_point(shape=1, size=2.5, color="black") +
+  geom_errorbar(aes(ymax=stats$Average + stats$StDev, ymin=stats$Average - stats$StDev)) +
+  scale_color_gradientn(colors=rainbow(6)) +
+  facet_wrap(~Strain)
+
+#extract integral growth parameter from dataset
+int=data.frame(results$integral.spline, results$reliability)
+
+#add information to int
+int=cbind(platemap, int)
+
+#add replicate number to data
+reps=data.frame(read.table("20160418_replicates.csv", header=TRUE))
+int=cbind(int, reps)
+
+#remove control values with replicate numbers
+normalization=subset(int, Concentration %in% 0)
+
+#remove unimportant columns
+normalization=normalization[,-1]
+normalization=normalization[,-2]
+normalization=normalization[,-2]
+
+#add negatives back
+int=inner_join(int, normalization, by = c("Strain", "Replicate"))
+
+#normalize data
+norm=int$results.integral.spline.x/int$results.integral.spline.y
+norm=data.frame(norm)
+
+#add information to normalized data
+int=cbind(int, norm)
+
+#group data
+grouped=group_by(int, Strain, Concentration)
+
+#remove tagged(bad) wells from data analysis
+grouped2=grouped[!(grouped$results.reliability.x=="FALSE"),]
+
+#Calculate average and stdev
+stats=summarise(grouped2, Average=mean(norm), StDev=sd(norm))
+
+#plot data
+ggplot(data=stats, aes(x=Concentration, y=Average)) +
+  geom_line(size=1) +
+  geom_point(aes(color=Concentration), size=2.5) +
+  geom_point(shape=1, size=2.5, color="black") +
+  geom_errorbar(aes(ymax=stats$Average + stats$StDev, ymin=stats$Average - stats$StDev)) +
+  scale_color_gradientn(colors=rainbow(6)) +
+  facet_wrap(~Strain)
---
+++ 20160421_GC_analysis.R
@@ -0,0 +1,70 @@
+library(reshape2)
+library(dplyr)
+library(ggplot2)
+
+# Read in the raw data and the platemap. You may need to first change your
+# working directory with the setwd command.
+rawdata <- read.table("20160421_01_AsV_MIC_I2727,47,A2705.asc")
+rawdata=t(rawdata)
+platemap <- read.csv("20160421_Platemap_High_MIC.csv")
+
+#add time and well infromation to data
+labels=read.csv("Time,wells.csv", header=FALSE)
+labels=t(labels)
+rawdata=cbind(labels,rawdata)
+rawdata=data.frame(rawdata, row.names=TRUE)
+rawdata=data.frame(t(rawdata))
+
+#remove s from time column
+rawdata$Time=gsub('s', '', rawdata$Time)
+
+# Reshape the data. Instead of rows containing the Time, Temperature,
+# and readings for each Well, rows will contain the Time, Temperature, a
+# Well ID, and the reading at that Well.
+reshaped <- melt(rawdata, id="Time", variable.name="Well",
+                 value.name="OD590")
+
+# Add information about the experiment from the plate map. For each Well
+# defined in both the reshaped data and the platemap, each resulting row
+# will contain the absorbance measurement as well as the additional columns
+# and values from the platemap.
+annotated <- inner_join(reshaped, platemap, by="Well")
+
+# Save the annotated data as a CSV for storing, sharing, etc.
+write.csv(annotated, "20160421_MIC_annotated.csv")
+
+conf_int95 <- function(data) {
+  n <- length(data)
+  error <- qt(0.975, df=n-1) * sd(data)/sqrt(n)
+  return(error)
+}
+
+#make OD590 a number
+OD590=as.numeric(annotated$OD590)
+annotated=cbind(annotated,OD590)
+annotated=annotated[,-3]
+
+#make time a number
+time=as.numeric(annotated$Time)
+annotated2=cbind(annotated,time)
+annotated=annotated2[,-1]
+
+# Group the data by the different experimental variables and calculate the
+# sample size, average OD600, and 95% confidence limits around the mean
+# among the replicates. Also remove all records where the Strain is NA.
+stats <- annotated %>%
+  group_by(Strain, Concentration, time) %>%
+  summarise(N=length(OD590),
+            Average=mean(OD590),
+            CI95=conf_int95(OD590)) %>%
+  filter(!is.na(Strain))
+
+# Plot the average OD600 over time for each strain in each environment
+ggplot(data=stats, aes(x=time/3600, y=Average, color=Concentration, group=Concentration)) +
+  geom_ribbon(aes(ymin=Average-CI95, ymax=Average+CI95, color=Concentration),
+              color=NA, alpha=0.3) +
+  geom_line(aes(color=Concentration)) +
+  scale_color_gradientn(colours=rainbow(8)) +
+  scale_y_log10() +
+  facet_wrap(~Strain, nrow=2) +
+  labs(x="Time (Hours)", y="Absorbance at 590 nm")
