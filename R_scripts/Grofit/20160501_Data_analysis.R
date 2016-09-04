1703efe625c235cb3862c591d5c1f3ce61b42166
dunivint <dunivint@msu.edu>
2016-09-04 14:39:24 -0400

    removed EC50 analysis

--- R_scripts/Grofit/20160501_Data_analysis.R
+++ R_scripts/Grofit/20160501_Data_analysis.R
@@ -68,12 +68,6 @@ results=data.frame(summary(results))
 #save results table
 write.csv(results, "20160501_results")
 
-#Find EC50
-EC50=drFit(results, control)
-EC50=data.frame(summary(EC50))
-
-#save EC50 results
-write.csv(EC50, "20160501_EC50")
 
 #extract maximum growth parameter from dataset
 mu=data.frame(results$mu.spline, results$reliability)
